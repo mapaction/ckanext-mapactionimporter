7dc40cd68d4152559597fb23b9f46ef31cc43584
Martin Burchell <martinb@aptivate.org>
2016-02-08 18:45:07 +0000

    Create resources when importing zip

--- ckanext/mapactionimporter/logic/action/create.py
+++ ckanext/mapactionimporter/logic/action/create.py
@@ -1,6 +1,8 @@
 from defusedxml.ElementTree import parse
 
 import os
+import cgi
+
 from slugify import slugify
 import tempfile
 import zipfile
@@ -12,14 +14,21 @@ def create_dataset_from_zip(context, data_dict):
     upload = data_dict.get('upload')
     map_package = upload.file
 
+    tempdir = tempfile.mkdtemp('-mapactionzip')
+
+    metadata_paths = []
+    file_paths = []
     with zipfile.ZipFile(map_package, 'r') as z:
-        metadata_path = [f for f in z.namelist()
-                         if f.endswith('.xml')][0]
-
-        tempdir = tempfile.mkdtemp('-mapactionzip')
         z.extractall(tempdir)
+        for f in z.namelist():
+            full_path = os.path.join(tempdir, f)
+            if f.endswith('.xml'):
+                metadata_paths.append(full_path)
+            else:
+                file_paths.append(full_path)
 
-    metadata_file = os.path.join(tempdir, metadata_path)
+    assert len(metadata_paths) == 1
+    metadata_file = metadata_paths[0]
 
     et = parse(metadata_file)
 
@@ -35,4 +44,39 @@ def create_dataset_from_zip(context, data_dict):
     dataset_dict['name'] = slugify(et.find('.//mapdata/ref').text)
     dataset = toolkit.get_action('package_create')(context, dataset_dict)
 
+    for resource_file in file_paths:
+        resource = {
+            'package_id': dataset['id'],
+            'path': resource_file,
+        }
+        _create_and_upload_local_resource(context, resource)
+
     return dataset
+
+
+def _create_and_upload_local_resource(context, resource):
+    path = resource['path']
+    del resource['path']
+    try:
+        with open(path, 'r') as the_file:
+            _create_and_upload_resource(context, resource, the_file)
+    except IOError:
+        msg = {'datapackage': [(
+            "Couldn't create some of the resources."
+            " Please make sure that all resources' files are accessible."
+        )]}
+        raise toolkit.ValidationError(msg)
+
+
+def _create_and_upload_resource(context, resource, the_file):
+    resource['url'] = 'url'
+    resource['url_type'] = 'upload'
+    resource['upload'] = _UploadLocalFileStorage(the_file)
+    toolkit.get_action('resource_create')(context, resource)
+
+
+class _UploadLocalFileStorage(cgi.FieldStorage):
+    def __init__(self, fp, *args, **kwargs):
+        self.name = fp.name
+        self.filename = fp.name
+        self.file = fp
--- ckanext/mapactionimporter/tests/logic/action/test_create.py
+++ ckanext/mapactionimporter/tests/logic/action/test_create.py
@@ -8,13 +8,25 @@ import ckanext.mapactionimporter.tests.helpers as custom_helpers
 class TestCreateDatasetFromZip(custom_helpers.FunctionalTestBaseClass):
 
     def test_it_allows_uploading_a_zipfile(self):
-	test_file = custom_helpers.get_test_file()
+        dataset = helpers.call_action(
+            'create_dataset_from_mapaction_zip',
+            upload=_UploadFile(custom_helpers.get_test_file()))
 
-        dataset = helpers.call_action('create_dataset_from_mapaction_zip',
-                                      upload=_UploadFile(test_file))
         nose.tools.assert_equal(
             dataset['title'],
             'Central African Republic:  Example Map- Reference (as of 3 Feb 2099)')
+
+        # Expect the JPEG And PDF referenced in the XML Metadata
+
+        dataset = helpers.call_action('package_show', id=dataset['id'])
+
+        resources = dataset['resources']
+
+        import pdb; pdb.set_trace()
+
+        nose.tools.assert_true(len(resources) == 2)
+        nose.tools.assert_equal(resources[0]['url_type'], 'upload')
+        #nose.tools.assert_true(resources[0]['name'] in resources[0]['url'])
 
 
 class _UploadFile(object):
