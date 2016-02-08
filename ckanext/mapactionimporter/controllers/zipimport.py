3614441182cea1866f10876d55d4f53169b26ed8
Martin Burchell <martinb@aptivate.org>
2016-02-08 16:47:58 +0000

    Add zip importer controller and test

--- ckanext/mapactionimporter/controllers/zipimport.py
+++ ckanext/mapactionimporter/controllers/zipimport.py
@@ -1,3 +1,4 @@
+import ckan.model as model
 import ckan.plugins.toolkit as toolkit
 
 
@@ -5,7 +6,11 @@ class ZipImportController(toolkit.BaseController):
     def new(self, data=None, errors=None, error_summary=None):
         errors = errors or {}
         error_summary = error_summary or {}
-        data = data or {}
+
+        default_data = {
+            'owner_org': toolkit.request.params.get('group'),
+        }
+        data = data or default_data
 
         return toolkit.render(
             'mapactionimporter/import_zip.html',
@@ -17,4 +22,33 @@ class ZipImportController(toolkit.BaseController):
         )
 
     def import_dataset(self):
-        pass
+        context = {
+            'model': model,
+            'session': model.Session,
+            'user': toolkit.c.user,
+        }
+        self._authorize_or_abort(context)
+
+        try:
+            params = toolkit.request.params
+            dataset = toolkit.get_action(
+                'create_dataset_from_mapaction_zip')(
+                    context,
+                    params,
+                )
+            toolkit.redirect_to(controller='package',
+                                action='read',
+                                id=dataset['name'])
+        except toolkit.ValidationError as e:
+            errors = e.error_dict
+            error_summary = e.error_summary
+            return self.new(data=params,
+                            errors=errors,
+                            error_summary=error_summary)
+
+    def _authorize_or_abort(self, context):
+        try:
+            toolkit.check_access('package_create', context)
+        except toolkit.NotAuthorized:
+            toolkit.abort(401,
+                toolkit._('Unauthorized to create a dataset'))
