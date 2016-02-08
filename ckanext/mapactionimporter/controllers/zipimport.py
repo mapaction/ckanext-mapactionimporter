320d12387dd9a42ce9354e74206527d52352fcbf
Martin Burchell <martinb@aptivate.org>
2016-02-08 10:39:43 +0000

    Fixed error when displaying upload form

--- ckanext/mapactionimporter/controllers/zipimport.py
+++ ckanext/mapactionimporter/controllers/zipimport.py
@@ -5,6 +5,7 @@ class ZipImportController(toolkit.BaseController):
     def new(self, data=None, errors=None, error_summary=None):
         errors = errors or {}
         error_summary = error_summary or {}
+        data = data or {}
 
         return toolkit.render(
             'mapactionimporter/import_zip.html',
